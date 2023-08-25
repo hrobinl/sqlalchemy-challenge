6a3801eb277e71ce56d322d29b69f9ddb0facfba
hrobinl <132854695+hrobinl@users.noreply.github.com>
2023-08-24 23:36:45 -0400

    adjustments

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,11 +1,14 @@
 # Import the dependencies.
-import numpy as np
+import numpy as n
+#import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 
-
+#import datetime as dt
+#from dateutil.relativedelta import relativedelta
 #################################################
 # Database Setup
 #################################################
@@ -16,15 +19,15 @@ Base = automap_base()
 
 # reflect the tables
 Base.prepare(engine, reflect=True)
-Base.classes.keys()
+#Base.classes.keys()
+
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+Session = Session(bind=engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
 #################################################
 # Flask Setup
 #################################################
@@ -39,10 +42,41 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
+        f"Welcome to the Climate App API. So glad to finally see a new face!<br>"
+        f"Here are the available routes:<br/>"
         f"Precipitation: /api/v1.0/precipitation<br/>"
         f"List of Stations: /api/v1.0/stations<br/>"
         f"Temperature for one year: /api/v1.0/tobs<br/>"
         f"Temperature stat from the start date(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd<br/>"
         f"Temperature stat from start to end dates(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd"
     )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Query to retrieve the last 12 months of precipitation data and return the results."""
+    # Create our session (link) from Python to the DB.
+    session = Session(engine)
+
+    # Calculate the date 1 year ago from the last data point in the database.
+    last_measurement_data_point_tuple = session.query(
+        Measurement.date).order_by(Measurement.date.desc()).first()
+    (latest_date, ) = last_measurement_data_point_tuple
+    latest_date = dt.datetime.strptime(latest_date, '%Y-%m-%d')
+    latest_date = latest_date.date()
+    date_year_ago = latest_date - relativedelta(years=1)
+
+    # Perform a query to retrieve the data and precipitation scores.
+    data_from_last_year = session.query(Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= date_year_ago).all()
+
+    session.close()
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    all_precipication = []
+    for date, prcp in data_from_last_year:
+        if prcp != None:
+            precip_dict = {}
+            precip_dict[date] = prcp
+            all_precipication.append(precip_dict)
+
+    # Return the JSON representation of dictionary.
+    return jsonify(all_precipication)
